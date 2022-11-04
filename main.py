bf29c29ae217400b05d3ccc00fea2cc0d34e5d35
Tenu <tenu2022@gmail.com>
2022-11-04 11:34:55 +0800

    新增

---
+++ main.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from datetime import datetime
+from tools import data
+
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        
+        self.label = tk.Label(self,text="",font=("arial",30))
+        self.label.pack(padx=50,pady=30)
+        self.change_time()
+        self.window_time()
+        
+
+        
+    def change_time(self):
+        now = datetime.now()
+        now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+        self.label.config(text=now_str)
+        self.after_id = self.label.after(1000,self.change_time)
+
+    def window_time(self):
+        print("計錄一次資料")
+        self.window_id = self.after(1000 * 60,self.window_time)
+
+    def delete_delay(self):
+        self.label.after_cancel(self.after_id)
+        self.after_cancel(self.window_id)
+        self.destroy()
+        
+        
+        
+
+
+def main():
+    window =  Window()
+    window.title("數位時鐘")
+    window.protocol("WM_DELETE_WINDOW",window.delete_delay)
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
