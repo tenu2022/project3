fd067f2f2e3147fea4ea2dbee21b5d1261b85220
Tenu <tenu2022@gmail.com>
2022-11-08 10:35:48 +0800

    建立檔案

--- tools/record.py
+++ tools/record.py
@@ -4,7 +4,7 @@ from datetime import datetime
 #建立record目錄
 directory = os.path.abspath("./record")
 if not os.path.isdir(directory):  # 取得絕對路徑，尚未建立前狀態為false ( not false = true )
-    os.makedirs(directory)  # true才會執行，建立record目錄
+    os.makedirs(directory)  # 建立record目錄，true才會執行
     # 一旦執行完第一次後，布林值就變成false，所以第二次後就永遠是false，不會再建立。
 
 
@@ -14,5 +14,7 @@ def recordData(distance,lightValue):
     filename = current_date.strftime("%Y-%m-%d.csv")  #將抓到的日期轉為文字str
     currentFiles = os.listdir(directory)  #使用listdir()顯示目錄內容
     if filename not in currentFiles:
-        print(f"沒有{filename}此檔")
+        #建立檔案
+        file = open(f"{directory}/{filename}",'w',encoding='utf-8')
+        file.close()
     
