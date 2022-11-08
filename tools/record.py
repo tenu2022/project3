264534f06e43778d8694683826351d11d0f9cdeb
Tenu <tenu2022@gmail.com>
2022-11-08 09:58:48 +0800

    建立絕對路徑

--- tools/record.py
+++ tools/record.py
@@ -1,3 +1,12 @@
+import os
+
+#建立record目錄
+directory = os.path.abspath("./record")
+if not os.path.isdir(directory):  # 取得絕對路徑，尚未建立前狀態為false ( not false = true )
+    os.makedirs(directory)  # true才會執行，建立record目錄
+    # 一旦執行完第一次後，布林值就變成false，所以第二次後就永遠是false，不會再建立。
+
+
 def recordData(distance,lightValue):
     print("紀錄")
     
